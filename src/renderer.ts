f5766e79ad5fe10721a11b3056b42e7105af8262
Lourens <lourensros@gmail.com>
2022-02-04 10:52:04 +0200

    Remove closeBrowser option since it is now obsolete. https://github.com/GoogleChrome/rendertron/pull/596/commits/d069d9df0f7e44bd4f6f4deb3339f9423934e074

--- src/renderer.ts
+++ src/renderer.ts
@@ -158,9 +158,6 @@ export class Renderer {
       // This should only occur when the page is about:blank. See
       // https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md#pagegotourl-options.
       await page.close();
-      if (this.config.closeBrowser) {
-        await this.browser.close();
-      }
       return { status: 400, customHeaders: new Map(), content: '' };
     }
 
@@ -168,9 +165,6 @@ export class Renderer {
     // https://cloud.google.com/compute/docs/storing-retrieving-metadata.
     if (response.headers()['metadata-flavor'] === 'Google') {
       await page.close();
-      if (this.config.closeBrowser) {
-        await this.browser.close();
-      }
       return { status: 403, customHeaders: new Map(), content: '' };
     }
 
@@ -227,9 +221,6 @@ export class Renderer {
     const result = (await page.content()) as string;
 
     await page.close();
-    if (this.config.closeBrowser) {
-      await this.browser.close();
-    }
     return {
       status: statusCode,
       customHeaders: customHeaders
@@ -288,9 +279,6 @@ export class Renderer {
 
     if (!response) {
       await page.close();
-      if (this.config.closeBrowser) {
-        await this.browser.close();
-      }
       throw new ScreenshotError('NoResponse');
     }
 
@@ -298,9 +286,6 @@ export class Renderer {
     // https://cloud.google.com/compute/docs/storing-retrieving-metadata.
     if (response.headers()['metadata-flavor'] === 'Google') {
       await page.close();
-      if (this.config.closeBrowser) {
-        await this.browser.close();
-      }
       throw new ScreenshotError('Forbidden');
     }
 
@@ -313,9 +298,6 @@ export class Renderer {
     // https://github.com/GoogleChrome/puppeteer/blob/v1.8.0/docs/api.md#pagescreenshotoptions
     const buffer = (await page.screenshot(screenshotOptions)) as Buffer;
     await page.close();
-    if (this.config.closeBrowser) {
-      await this.browser.close();
-    }
     return buffer;
   }
 }
